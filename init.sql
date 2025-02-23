bba2970a3597df0da622e6ea6fc394bff3fa8884
JoackoF <fuentesmo.jaf@gmail.com>
2025-02-23 17:18:49 -0600

    Db hash bug correction

--- init.sql
+++ init.sql
@@ -3,5 +3,3 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
-
-INSERT INTO users (username, password) VALUES ('admin', 'admin123');
